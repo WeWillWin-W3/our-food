79558d4c5d8a5971a9399a093c9b4e9765c56e2f
Lucas Barbosa <llbarbosasc@gmail.com>
2020-09-21 22:16:51 -0400

    feat(web): styled-components e css reset

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import { GlobalStyle } from './styles/GlobalStyle'
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>Hello, world!</h1>
+      </div>
+      <GlobalStyle/>
+    </>
   )
 }
 

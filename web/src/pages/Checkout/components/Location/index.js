39a94e6df2cd62ac51f1dce49fa290297a0cf215
LucasMoro01 <lucasmoro_07@outlook.com>
2020-10-05 23:10:42 -0300

    Criei cartão de localização

---
+++ web/src/pages/Checkout/components/Location/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ImgPlaceholder } from '../../../../components/ImgPlaceholder'
+
+import { Card, CardPrincipal, CardNameStreet, CardComplement, CardCity } from './styled'
+
+export const Location = () => (
+    <Card>
+        <ImgPlaceholder width={80} height={80} style={{ margin: '0px 5px 0px 0px' }}/>
+        <CardPrincipal>
+            <CardNameStreet>Nome da rua, n°</CardNameStreet>
+            <CardComplement>Complemento</CardComplement>
+            <CardCity>Cidade</CardCity>
+        </CardPrincipal>
+    </Card>
+)

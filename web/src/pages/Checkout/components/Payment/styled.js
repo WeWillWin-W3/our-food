853b6196a615cf79d9f80a8cf46894621019ed61
LucasMoro01 <lucasmoro_07@outlook.com>
2020-10-08 10:22:11 -0300

    Criei card de Pagamento

---
+++ web/src/pages/Checkout/components/Payment/styled.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Box = styled.div`
+    padding: 15px 15px 15px 15px;
+    border-radius: 9px;
+    background-color: white;
+    width: 28vw;
+    margin-bottom: 15px;
+`
+
+export const Input = styled.input`
+    outline: none;
+    name='username';
+    border: none;
+    background-color: transparent;
+    variant="filled"
+    margin-left: 15px;
+    font-size: 14px;
+`
+
+export const InputBox = styled.div`
+    align-items: center;
+    border-color: #B2B2B2;
+    padding: 18px 20px;
+    border-radius: 9px;
+    border-style: solid;
+    border-width: 2px;
+    margin-top: 10px;
+    color: #707070;
+    font-size: 20px;
+`

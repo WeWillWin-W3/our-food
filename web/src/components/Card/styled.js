2e3802849ec9f1339a499237d856d55ab8f22db9
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-10-09 14:44:32 -0400

    Removi bibliotecas não utilizadas

--- web/src/components/Card/styled.js
+++ web/src/components/Card/styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export const CardContainer = styled.div`
     padding: 74px;
--- web/src/pages/StoreInformation/styled.js
+++ web/src/pages/StoreInformation/styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export const Main = styled.main`
     display: flex;

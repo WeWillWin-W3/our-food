d2ef7229fd6ef1dffb34b650f09b75925fc41297
Lucas Barbosa <llbarbosasc@gmail.com>
2020-11-27 16:34:01 -0400

    docs: detalhamento das histórias de usuário pendentes

--- web/src/providers/APIProvider/index.js
+++ web/src/providers/APIProvider/index.js
@@ -6,26 +6,40 @@ const APIProviderContext = createContext({})
 const APIProvider = props => {
     const [state, setState] = useState({
         user: undefined,
+        authToken: undefined,
         error: undefined,
         loading: false
     })
     const updateState = (nextState) => setState({ ...state, ...nextState })
 
-    const createUser = async (userData) => {
-        updateState({ error: undefined, loading: true })
+    const logout = () => updateState({ user: undefined })
 
-        try {
-            const user = await API.createUser(userData)
-            updateState({ user: user, loading: false })
-        } catch (err) {
-            updateState({ error: err.response.data, loading: false })
+    const createUser = async (userData) => {
+        updateState({ error: undefined })
+
+        if (!state.user) {
+            updateState({ loading: true })
+
+            try {
+                const user = await API.createUser(userData)
+                updateState({ user: user, loading: false })
+
+                // TODO: Autenticar o usuário e obter o authToken
+            } catch (err) {
+                updateState({ error: err.response.data, loading: false })
+            }
+
+            return;
         }
+
+        updateState({ error: "O usuário está logado. Faça logout primeiro" })
     }
 
     return (
         <APIProviderContext.Provider value={{
             ...state,
             createUser,
+            logout
         }} {...props} />
     )
 }
